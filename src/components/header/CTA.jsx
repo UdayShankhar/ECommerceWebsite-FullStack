cd613fe2ce35ba98b9f5c018e25779bccd05fc70
Uday <shankhar87@gmail.com>
2022-04-22 22:05:26 +0530

    final commit

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CTA() {
   return (
     <div className='cta'>
-<a href='' download className='btn'>Download CV</a>
+{/* <a href='' download className='btn' style={{letterSpacing:'0.1rem'}} >View CV</a> */}
 <a href='#contact' className='btn btn-primary'>Let's talk</a>
     </div>
   )
