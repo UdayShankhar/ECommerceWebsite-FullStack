66271d7475af9cbc765b6a878ba70680aa4e908b
Uday Shankhar <uday.shankhar@bloomsmobility.com>
2023-01-20 16:13:28 +0530

    Experience update

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -7,28 +7,27 @@ function Experience() {
       <h2>Experience</h2>
       <div className='container experience-container'>
         <div className='experience-first'>
-          <h3>Trainee Engineer</h3>
-          <h5>Candid Machines</h5>
-          <h5>September 2020 - August 2021</h5>
+          <h3>Full Stack Developer</h3>
+          <h5>Blooms Mobility Technologies</h5>
+          <h5>May 2022 - Present</h5>
           <p>Job Description</p>
           <ul className='desc-list'>
-            <li>Designed 15+ AutoCAD projects for our most-important clients</li>
-            <li>Developed various HMI interface for our overseas clients </li>
-            <li>Worked as a PLC programmer for various projects</li>
+            <li>Implemented Redux from scratch to the whole application for a better state management.</li>
+            <li>Converted the whole Angular 8 application into a PWA app for an enhanced performance. </li>
+            <li>Collaborated with product team members to implement new features in the application</li>
           </ul>
         </div>
+
         <div className='experience-second'>
-          <h3>Business Development Associate</h3>
-          <h5>Napa's Handlooms</h5>
-          <h5>June 2018 - September 2020</h5>
+          <h3>Full Stack Developer Intern</h3>
+          <h5>Guvi Geeks Pvt.Ltd</h5>
+          <h5>December 2021 - May 2022</h5>
           <p>Job Description</p>
           <ul className='desc-list'>
-            <li>Worked for marketing the products through online which increased the sales of the organization by 35%</li>
-            <li>Responsible for tracking and processing online orders</li>
-            <li>Monitor and improved business performance</li>
+            <li>Developed and designed eye-catching UI for E-Commerce website and created a secure login.</li>
+            <li>Implemented new website features including logic, data filters and pricing models.</li>
           </ul>
         </div>
-        
       </div>
     </section>
   )
